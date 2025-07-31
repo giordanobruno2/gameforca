3ca8e195b6e4f297931391323cf7d3555e974af0
Giordano Bruno <giordanobr2@gmail.com>
2025-07-31 12:38:46 -0300

    imgs tela

--- src/main.py
+++ src/main.py
@@ -4,7 +4,19 @@ import flet as ft
 def main(page: ft.Page):
     page.bgcolor = ft.Colors.BROWN_600
     
+    cena = ft.Image(col=12, src='images/scene.png')
     
-    page.update()
+    
+    layout = ft.ResponsiveRow(
+        controls=[
+            cena,
+            # jogo,
+            # teclado,
+            cena           
+        ],        
+    )
+    
+    page.add(layout)
+
 if __name__ == '__main__':
-    ft.app(target=main)
+    ft.app(target=main, assets_dir='assets')
