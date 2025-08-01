fdfd3bb4dc4d85be6a7ee058491c0d06ab01563b
Giordano Bruno <giordanobr2@gmail.com>
2025-07-31 21:26:56 -0300

    31_07 21:26

--- src/main.py
+++ src/main.py
@@ -15,13 +15,35 @@ def main(page: ft.Page):
         
         )
     
+    escolha = 'flet'
+    palavra = ft.Row(
+        controls=[
+            ft.Container(
+                bgcolor=ft.Colors.AMBER,
+                width=50,
+                height=50,
+                content=ft.Text(
+                    value=letra,
+                    color=ft.Colors.WHITE,
+                    size=30,
+                    text_align=ft.TextAlign.CENTER,
+                    weight=ft.FontWeight.BOLD
+                )
+            ) for letra in escolha
+        ]
+    )
+    
+
+    
     jogo = ft.Container(
         col = {'xs': 12, 'lg': 6},
         padding = ft.padding.all(50),
         content = ft.Column(
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             controls=[
                 vitima,
-                # palavra
+                palavra
             ]
         )
         
@@ -33,6 +55,8 @@ def main(page: ft.Page):
         padding= ft.padding.only(top=150, left=80, right=80, bottom=50),
         content = ft.Row(
             wrap=True,
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
             controls=[
                 ft.Container(
                     height=50,
